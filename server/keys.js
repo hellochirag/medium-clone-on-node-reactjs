e514ea803bd514559254fe0c074a31c920cf7cba
ChiragJadav-Bacancy <chirag.jadav@bacancytechnology.com>
2018-07-27 13:05:31 +0530

    Task Meduim clone Repo Inital Commit

---
+++ server/keys.js
@@ -0,0 +1,14 @@
+// add this file to .gitinore
+module.exports = {
+    cloudinary: {
+        cloud_name: 'diej30ftz',
+        api_key: '726419836564348',
+        api_secret: 'xzXYaotUDwkjU1SjYePjFqmIK3E'
+    },
+    mongodb: {
+        dbURL: 'mongodb://radhe:radhe1@ds153841.mlab.com:53841/meduim-clone-node'
+    },
+    session: {
+        cookieKey: 'heyamchirag'
+    }
+};
